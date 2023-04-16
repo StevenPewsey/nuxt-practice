e395111ecdc19120a8337fc6882e3f0c142cfefa
Steve Pewsey <steven@thriva.co>
2023-04-16 21:45:15 +0100

    delete stuff

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-// import { useQuery } from "@tanstack/vue-query"
-
 definePageMeta({ auth: false })
 
 const { data: authData } = useAuth()
@@ -8,24 +6,10 @@ const { data: authData } = useAuth()
 const welcomeText = computed(() => {
   return authData.value?.user?.name ?? "unauthenticated user"
 })
-
-// const count = ref(1)
-// const double = computed(() => count.value * 2)
-
-// const fetcher = async () =>
-//   await fetch("https://jsonplaceholder.typicode.com/posts/1").then((response) =>
-//     response.json()
-//   );
-
-// const { data, suspense } = useQuery({ queryKey: ["test"], queryFn: fetcher});
-
-// await suspense();
 </script>
 
 <template>
   <div>
     <h1 class="text-3xl">Welcome {{ welcomeText }}</h1>
-
-    <div v-if="authData?.accessToken">{{ authData.accessToken }}</div>
   </div>
 </template>
